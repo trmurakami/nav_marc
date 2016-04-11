96c94d78d85da1f0338c282f304b377e0553ae32
Tiago Murakami <trmurakami@gmail.com>
2016-04-11 10:46:38 -0300

    Correção no importador

--- sh/convert_import_alephseq.sh
+++ sh/convert_import_alephseq.sh
@@ -3,4 +3,4 @@
 rm ../data/records.json
 catmandu convert MARC --type ALEPHSEQ to JSON < $1 --fix ../fixes/fixes.txt >> ../data/records.json
 #catmandu import JSON to MongoDB --database_name sibi --bag producao < ../data/records.json
-mongoimport --db sibi --collection producao --file ../data/records.json --batchSize 1 --upsert
+mongoimport --db sibi --collection producao --file ../data/records.json --batchSize 1 --upsert --jsonArray
