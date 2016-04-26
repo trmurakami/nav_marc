d6c6856126e24d2450237f5ebb9d7508ab1595a6
Tiago Murakami <trmurakami@gmail.com>
2016-04-26 07:36:23 -0300

    Multi processamento

--- sh/convert_import_alephseq.sh
+++ sh/convert_import_alephseq.sh
@@ -2,6 +2,6 @@
 #catmandu import MARC --type ALEPHSEQ to MongoDB  --fix ../fixes/fixes.txt --database_name sibi --bag producao < ../data/prodeca.seq
 rm ../data/records.json
 sort $1 > ../data/records.seq
-catmandu convert MARC --type ALEPHSEQ to JSON < ../data/records.seq --fix ../fixes/fixes.txt >> ../data/records.json
+catmandu convert MARC --type ALEPHSEQ to JSON --line_delimited 1 < ../data/records.seq --fix ../fixes/fixes.txt >> ../data/records.json
 #catmandu import JSON to MongoDB --database_name sibi --bag producao < ../data/records.json
 mongoimport --db sibi --collection producao --file ../data/records.json --batchSize 1 --upsert #--jsonArray
