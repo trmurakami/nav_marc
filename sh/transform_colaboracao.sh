30b1a4dc754822edb9968750ad8a7e918d7dc4ec
Tiago Murakami <trmurakami@gmail.com>
2016-05-09 10:59:27 -0300

    Contador para acompanhamento do script de colaboração

--- sh/transform_colaboracao.sh
+++ sh/transform_colaboracao.sh
@@ -21,8 +21,8 @@ IFS=$'\n'       # make newlines the only separator
 for line in $(cat $1);
 do
 
-echo $i
-i=(($i+1))
+  echo $i
+  i=$((i+1))
 
 line=$(printf "%s\n" "$line" | sed "s/\"\",\"\"/|/g" | sed 's/,\"\[\"\"/#/g' | sed 's/\"\"\]\"//g' )
 _id=$(printf "%s\n" "$line" | cut -d "#" -f 1 | sed 's/\"//g')
